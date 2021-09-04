2cf4e991ab5f3ab2d7503b3a216d29f3cb0b172b
raouftams <raouftams@gmail.com>
2021-09-04 16:45:04 +0100

    design update

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///takerboust.db'
 db = SQLAlchemy(app)
 
-
-
-
+"""Landing page route"""
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/addStudent', methods=['POST', 'GET'])
+def create_student():
+    if request.method == 'POST':
+        pass
+
+    return render_template('formulaire.html')
+
+@app.route('/stats')
+def stats():
+    return "statistics page"
 
 if __name__ == "__main__":
     app.run(debug=True)
