106287856ff2c99349ad382ca7f9f095ae773873
raouftams <raouftams@gmail.com>
2021-09-07 18:32:28 +0100

    update form design

--- static/js/formulaire.js
+++ static/js/formulaire.js
@@ -1,14 +1,22 @@
 const loadStudentForm = () => {
     let employeCard = document.getElementById("employeCard");
     let studentCard = document.getElementById("studentCard");
+    let employeForm = document.getElementById("employeForm");
+    let studentForm = document.getElementById("studentForm");
     employeCard.classList.remove("card-active")
     studentCard.classList.add("card-active")
+    employeForm.classList.add("hide-form")
+    studentForm.classList.remove("hide-form")
 }
 
 const loadEmployeForm = () => {
     let employeCard = document.getElementById("employeCard");
     let studentCard = document.getElementById("studentCard");
+    let employeForm = document.getElementById("employeForm");
+    let studentForm = document.getElementById("studentForm");
     employeCard.classList.add("card-active")
     studentCard.classList.remove("card-active")
+    employeForm.classList.remove("hide-form")
+    studentForm.classList.add("hide-form")
 }
 
